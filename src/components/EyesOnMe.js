2c4b0edaac643afca6fb070ac6cd58acbdecfb2e
Erin Yant <ghosrider383@gmail.com>
2024-04-24 16:13:52 -0600

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    const handleFocus = () => {
+        console.log('Good!')
+        console.log('Focused')
+    }
+    const handleBlur = () => {
+        console.log('Hey! Eyes on me!')
+        console.log('Blurred')
+    }
+    
+    return(
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
